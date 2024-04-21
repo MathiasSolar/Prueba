260a0cc203a2f5705dad13b4c3bc65faded0da7d
Mathias <mathiassolar24@gmail.com>
2024-04-21 16:09:03 -0400

    primera prueba

---
+++ lib/prueba.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PruebaApp extends StatelessWidget {
+  const PruebaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: const Text('prueba github'),));
+  }
+}
